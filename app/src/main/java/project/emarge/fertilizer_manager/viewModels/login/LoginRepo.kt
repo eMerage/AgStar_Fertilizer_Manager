c79fee12fe98ef60f8b80b1d9cfc8109566afeca
eMergeSolutionPVT <emerge_github@friendtho.com>
2019-09-18 17:26:27 +0530

    c

--- app/src/main/java/project/emarge/fertilizer_manager/viewModels/login/LoginRepo.kt
+++ app/src/main/java/project/emarge/fertilizer_manager/viewModels/login/LoginRepo.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
 import android.net.ConnectivityManager
+import android.widget.Toast
 import androidx.databinding.ObservableField
 import androidx.lifecycle.MutableLiveData
 import com.google.android.gms.tasks.OnCompleteListener
@@ -24,7 +25,8 @@ import project.emarge.fertilizer_manager.services.network.NetworkErrorHandler
 class LoginRepo(application: Application) {
 
 
-    var encryptedPreferences: EncryptedPreferences=EncryptedPreferences.Builder(application).withEncryptionPassword("122547895511").build()
+    var encryptedPreferences: EncryptedPreferences =
+        EncryptedPreferences.Builder(application).withEncryptionPassword("122547895511").build()
     private val USER_ID = "userID"
     private val USER_NAME = "userName"
     private val USER_IMAGE = "userImage"
@@ -36,7 +38,10 @@ class LoginRepo(application: Application) {
     var apiInterface: APIInterface = ApiClient.client(application)
 
 
-    fun loginValidationRepo(userName: MutableLiveData<String>, password: MutableLiveData<String>): MutableLiveData<String> {
+    fun loginValidationRepo(
+        userName: MutableLiveData<String>,
+        password: MutableLiveData<String>
+    ): MutableLiveData<String> {
         var validationErrorMsg = MutableLiveData<String>()
 
         var error: String = when {
@@ -54,7 +59,7 @@ class LoginRepo(application: Application) {
 
     fun getAppVersionRepo(): MutableLiveData<String> {
         var appVersion = MutableLiveData<String>()
-        var appLevel: String =app.getString(R.string.app_name)
+        var appLevel: String = app.getString(R.string.app_name)
         var pInfo: PackageInfo? = null
         try {
             pInfo = app.packageManager.getPackageInfo(app.packageName, 0)
@@ -77,8 +82,11 @@ class LoginRepo(application: Application) {
 
 
     fun getUserDetails(
-        user: MutableLiveData<String>, password: MutableLiveData<String>, isRememberMeChecked: Boolean,
-        isLoading: ObservableField<Boolean>, buttonVisibale: ObservableField<Boolean>
+        user: MutableLiveData<String>,
+        password: MutableLiveData<String>,
+        isRememberMeChecked: Boolean,
+        isLoading: ObservableField<Boolean>,
+        buttonVisibale: ObservableField<Boolean>
     ): MutableLiveData<Rep> {
         val data = MutableLiveData<Rep>()
         var loginUser = Rep()
@@ -86,55 +94,70 @@ class LoginRepo(application: Application) {
         isLoading.set(true)
         buttonVisibale.set(false)
 
-        FirebaseInstanceId.getInstance().instanceId.addOnCompleteListener(OnCompleteListener { task ->
+        try {
+            FirebaseInstanceId.getInstance()
+                .instanceId.addOnCompleteListener(OnCompleteListener { task ->
 
-            apiInterface.validateUser(user.value.toString(), password.value.toString(), 1, task.result?.token!!)
-                .subscribeOn(Schedulers.io())
-                .doOnError { it }
-                .doOnTerminate { }
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(object : Observer<Rep> {
-                    override fun onSubscribe(d: Disposable) {
-                    }
-
-                    override fun onNext(log: Rep) {
-                        loginUser = log
-                        data.postValue(loginUser)
-                        if (isRememberMeChecked) {
-                            encryptedPreferences.edit().putBoolean(USER_REMEMBER, true).apply()
-                        } else {
-                           encryptedPreferences.edit().putBoolean(USER_REMEMBER, false).apply()
+                apiInterface.validateUser(
+                    user.value.toString(),
+                    password.value.toString(),
+                    1,
+                    task.result?.token!!
+                )
+                    .subscribeOn(Schedulers.io())
+                    .doOnError { it }
+                    .doOnTerminate { }
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(object : Observer<Rep> {
+                        override fun onSubscribe(d: Disposable) {
                         }
 
-                    }
-
-                    override fun onError(e: Throwable) {
-                        isLoading.set(false)
-                        buttonVisibale.set(true)
-                        loginUser.loginNetworkError = networkErrorHandler(e)
-                        data.postValue(loginUser)
-                    }
-
-                    override fun onComplete() {
-                        if(loginUser.userStatus){
-                            encryptedPreferences.edit().putInt(USER_ID, loginUser.userID).apply()
-                            encryptedPreferences.edit().putString(USER_NAME, loginUser.name).apply()
-                        }else{
+                        override fun onNext(log: Rep) {
+                            loginUser = log
+                            data.postValue(loginUser)
+                            if (isRememberMeChecked) {
+                                encryptedPreferences.edit().putBoolean(USER_REMEMBER, true).apply()
+                            } else {
+                                encryptedPreferences.edit().putBoolean(USER_REMEMBER, false).apply()
+                            }
 
                         }
-                        isLoading.set(false)
-                        buttonVisibale.set(true)
 
-                    }
-                })
-        })
+                        override fun onError(e: Throwable) {
+                            isLoading.set(false)
+                            buttonVisibale.set(true)
+                            loginUser.loginNetworkError = networkErrorHandler(e)
+                            data.postValue(loginUser)
+                        }
+
+                        override fun onComplete() {
+                            if (loginUser.userStatus) {
+                                encryptedPreferences.edit().putInt(USER_ID, loginUser.userID)
+                                    .apply()
+                                encryptedPreferences.edit().putString(USER_NAME, loginUser.name)
+                                    .apply()
+                            } else {
+
+                            }
+                            isLoading.set(false)
+                            buttonVisibale.set(true)
+
+                        }
+                    })
+            })
+        } catch (ex: Exception) {
+            Toast.makeText(app, "Login fail,Please try again", Toast.LENGTH_LONG).show()
+        }
+
+
 
         return data
     }
 
 
     fun Context.isConnectedToNetwork(): Boolean {
-        val connectivityManager = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
+        val connectivityManager =
+            this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
         return connectivityManager?.activeNetworkInfo?.isConnectedOrConnecting() ?: false
     }
 
